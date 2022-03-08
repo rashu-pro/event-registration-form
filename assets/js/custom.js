b224ae893495035d3f1711b1bd04ce968acd64f0
rashuNath <rashu.web@gmail.com>
2022-03-08 18:49:41 +0600

    initial commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,23 @@
  */
 
 console.log('way to go!');
+
+$('.quantity-increase').on('click',function () {
+    var self = $(this),
+        quantitySelector = self.closest('.quantity-wrap').find('.form-control'),
+        quantityValue = quantitySelector.val();
+
+    if(quantityValue<10){
+        quantitySelector.val(parseInt(quantityValue)+1);
+    }
+});
+
+$('.quantity-decrease').on('click',function () {
+    var self = $(this),
+        quantitySelector = self.closest('.quantity-wrap').find('.form-control'),
+        quantityValue = quantitySelector.val();
+
+    if(quantityValue>0){
+        quantitySelector.val(parseInt(quantityValue)-1);
+    }
+});
